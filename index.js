4d466b34b28f8529eb00bd2b9a0365fdc5be9ed9
talha-hassan <talha.hammer@gmail.com>
2023-04-07 07:21:54 +0500

    asd

--- index.js
+++ index.js
@@ -20,6 +20,27 @@ var destination = { latitude: 44.464985952017166, longitude: 26.068382554630922 
 // geocoder.reverse({ lat: destination.latitude, lon: destination.longitude }).then(x=>{
 //     console.log(x)
 // })
+function distanceCalculation(lat1, lon1, lat2, lon2, unit) {
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+        return 0;
+    }
+    else {
+        var radlat1 = Math.PI * lat1/180;
+        var radlat2 = Math.PI * lat2/180;
+        var theta = lon1-lon2;
+        var radtheta = Math.PI * theta/180;
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180/Math.PI;
+        dist = dist * 60 * 1.1515;
+        if (unit=="K") { dist = dist * 1.609344 }
+        if (unit=="N") { dist = dist * 0.8684 }
+        return dist;
+    }
+}
 const sendNotfication = (value)=>{
     var message = {
         to : value,
@@ -54,9 +75,14 @@ app.get('/getDestination',(req,res)=>{
     console.log(destination)
     res.send(destination).status(200)
 })
-app.post('/changeDestination',(req,res)=>{
-    if(req.body.latitude & req.body.longitude){
-        destination = req.body
+app.post('/changeDestination',(req,res)=>{  
+    console.log(destination)
+    if(req.body?.latitude && req.body?.longitude){
+        destination = {
+            latitude : Number(req.body.latitude),
+            longitude : Number(req.body.longitude)
+        }
+        console.log("destination:" ,destination)
         res.sendStatus(201)
         return ;
     }
@@ -71,10 +97,10 @@ app.post('/location',(req,res)=>{
     if(resp != {}){
         data[Object.keys(resp)[0]] = resp[Object.keys(resp)[0]]
     }
-    const latdiff = Math.abs(resp[Object.keys(resp)[0]].latitude - destination.latitude)
-    const longdif = Math.abs(resp[Object.keys(resp)[0]].longitude - destination.longitude)
-    console.log(latdiff,longdif)
-    if(latdiff < 0.000021 || longdif < 0.000021){
+    const kilometers = distanceCalculation(destination.latitude,destination.longitude,resp[Object.keys(resp)[0]].latitude,resp[Object.keys(resp)[0]].longitude,'K')
+    
+    console.log("Kilometers",kilometers)
+    if(kilometers < 0.003){
         sendNotfication(Object.keys(resp)[0])
     }
     res.sendStatus(200)
